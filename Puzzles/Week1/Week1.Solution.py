9672112623f2fdc4d7cf49b113bdee3304d77bf3
jhmalpern <jhmalpern@gmail.com>
2021-02-20 17:05:41 -0500

    Updated week 1 to be faster by keeping running tally of floor.

--- Puzzles/Week1/Week1.Solution.py
+++ Puzzles/Week1/Week1.Solution.py
@@ -12,14 +12,17 @@ def main():
     startTime = time.time()
 
     # Step 1: Read in .txt file
-    # Step 2: Use regular expressions to create list of occurences of '(' and ')'
-    # Step 3: Find difference in length of those lists
+    # Step 2: Keep running tally of floor. Much faster than finding the difference at the end
     with open('PuzzleString.txt','r') as f:
         puzzleInput = f.read()
-    up = ([i for i in range(len(puzzleInput)) if puzzleInput.startswith('(',i)])
-    down = ([i for i in range(len(puzzleInput)) if puzzleInput.startswith(')',i)])
-    print(len(up)-len(down))
+
     print("It took %s to execute this solution" % (round(time.time()-startTime, 4)))
+
+    startTimeRunningTally = time.time()
+    floor=0 #Starts at ground (floor = 0)
+    for i in range(len(puzzleInput)):
+        floor = floor + 1 if puzzleInput[i] == "(" else  floor-1
+    print("Finished on floor %s" % (floor))
 
 if __name__ == "__main__":
     main()
