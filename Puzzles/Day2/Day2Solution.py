f95e8e8eac42fdffa2a6e3fb3f6325db2a4881ae
jhmalpern <jhmalpern@gmail.com>
2021-02-28 17:14:31 -0500

    added benchmarking for Day2

--- Puzzles/Day2/Day2Solution.py
+++ Puzzles/Day2/Day2Solution.py
@@ -3,32 +3,40 @@ import time
 
 def main():
     startTime = time.time()
-    with open("PuzzleInput.txt",'r') as f:
-        puzzleInput = f.read().splitlines()    
+    #with open("PuzzleInput.txt",'r') as f:
+    #    puzzleInput = f.read().splitlines()    
 
     #####   PART 1   #####
-    #For Loop approach
-    wrapNeeded = 0
-    for i  in range(len(puzzleInput)):
-        l, w, h = [int(i) for i in puzzleInput[i].split('x')]
-        area = 2*l*w + 2*w*h + 2*l*h
-        wrapNeeded = wrapNeeded + area + min(l*w,w*h,l*h)
-
+    benchTime = []
+    for j in range(1000):
+        startTime = time.time()
+        with open("PuzzleInput.txt",'r') as f:
+            puzzleInput = f.read().splitlines() 
+        wrapNeeded = 0
+        for i  in range(len(puzzleInput)):
+            l, w, h = [int(i) for i in puzzleInput[i].split('x')]
+            area = 2*l*w + 2*w*h + 2*l*h
+            wrapNeeded = wrapNeeded + area + min(l*w,w*h,l*h)
+        benchTime.append(time.time()-startTime)
     print("Elves need %s feet of wrapping paper" % (wrapNeeded))
-    print("It took %s to execute this solution" % (round(time.time()-startTime, 4)))
+    print("On average, it took %s seconds to complete Part1" % (sum(benchTime)/len(benchTime)))
 
     #####   PART 2   #####
-    # They need a present that is the shortest perimeter around one face
-    # Plus the cubic feet (product of all sides)
-    startTimeRibbon = time.time()
-    bow = 0
-    wrap = 0
-    for i in range(len(puzzleInput)):
-        l, w, h = [int(i) for i in puzzleInput[i].split('x')]
-        bow = (l*w*h)
-        wrap = wrap + bow + min(2*(l+h), 2*(l+w), 2*(w+h))
+    benchTime2 = []
+    for j in range(1000):
+        startTime = time.time()
+        with open("PuzzleInput.txt",'r') as f:
+            puzzleInput = f.read().splitlines() 
+        bow = 0
+        wrap = 0
+        for i in range(len(puzzleInput)):
+            l, w, h = [int(i) for i in puzzleInput[i].split('x')]
+            bow = (l*w*h)
+            wrap = wrap + bow + min(2*(l+h), 2*(l+w), 2*(w+h))
+        benchTime2.append(time.time()-startTime)
+
     print("Elves need %s feet of ribbon" % (wrap))
-    print("It took %s to execute this solution" % (round(time.time()-startTimeRibbon, 4)))
+    print("On average, it took %s seconds to complete Part2" % (sum(benchTime2)/len(benchTime2)))
 
 
 if __name__ == "__main__":
